c235b42cd34de4e4af5dd32e53131a84be91c68c
ahsia3 <tonyhsia22@gmail.com>
2019-12-14 16:26:11 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/HKP/precache-manifest.8503fbe28054367611a0e553f64696c0.js"
+  "/HKP/precache-manifest.3a05e24a64b24c356f9268cef8b4ec88.js"
 );
 
 self.addEventListener('message', (event) => {
